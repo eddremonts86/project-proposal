d95897cbeae361e365d78db0e58d35cc53dfced0
Edd Remonts <eduardo.inerarte@gmail.com>
2024-07-19 10:14:24 +0200

    chore: Update dependencies and configuration files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,31 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'turbo',
+    'prettier',
+    'plugin:tailwindcss/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'tailwindcss'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/classnames-order': 'error',
   },
+  settings: {
+    tailwindcss: {
+      callees: ['cn', 'cva'],
+      config: 'tailwind.config.cjs',
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+    },
+  ],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
   resolve: {
-    alias: [{
-      find: "@",
-      replacement: path.resolve(path.join(__dirname, './src')),
-    }],
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(path.join(__dirname, './src')),
+      },
+    ],
   },
 })
