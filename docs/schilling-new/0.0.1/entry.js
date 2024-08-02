5618ef28ed7d3fbff60176fc4c76ed4825601f38
Edd Remonts <eduardo.inerarte@gmail.com>
2024-08-02 11:34:53 +0200

    test2

---
+++ docs/schilling-new/0.0.1/entry.js
@@ -0,0 +1,32 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../../../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../../../src/components/globals/Table/components/filters/TableFilters.tsx';
+reactComponents['TableFiltersProps'] = Component0;
+
+import Component1 from '../../../src/components/containers/FormGroupsContainer.tsx';
+reactComponents['exports'] = Component1;
+
+import Component2 from '../../../src/components/globals/Table/components/actions/TableLoading.tsx';
+reactComponents['exports'] = Component2;
